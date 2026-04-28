03a639cef6e4de0bdb9c237d5b488cb939e91ab1
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-04-28 08:03:41 -0500

    add diagnostics to tutorial

--- tutorial/run_3dvar_ursa.sh
+++ tutorial/run_3dvar_ursa.sh
@@ -34,17 +34,14 @@ cp -r ${cdir}/input_yaml/jedi_3dvar_fv3* ${exp_dir_path}
 # Sym-link input directories
 ln -nsf ${JEDI_INPUT_PATH}/* ${exp_dir_path}
 
-# Copy diagnostic plotting scripts to experimental case directory
-cp -r ${cdir}/diagnostics ${exp_dir_path}
+# Copy diagnostics to experimental case directory (reference plot dir is excluded)
+rsync -av --exclude 'diag-results' ${cdir}/diagnostics ${exp_dir_path}
 
 # Move to experimental case directory
 cd ${exp_dir_path}
 
 # Create output directory
 mkdir -p output
-
-# Create plot_output directory
-mkdir -p plot_output
 
 # Load modules
 module purge
