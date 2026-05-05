17f87ef3a018650e4321ab4d7ef5436503c19138
chan-hoo <chan-hoo.jeon@noaa.gov>
2026-05-05 08:26:18 -0500

    rename tutorial and remove py_env

--- tutorial/py_env/build_venv.sh
+++
@@ -1,8 +0,0 @@
-module purge
-module load python
-rm -rf cadre_pyenv
-python3 -m venv cadre_pyenv
-source cadre_pyenv/bin/activate
-pip install --upgrade pip
-pip install -r cadre_env_list.txt
-pip list
